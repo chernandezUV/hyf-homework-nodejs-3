ce2b1c375ec132149ae31d2c170a2cda08ee915c
chernandezUV <chernandez@unitedvirtualities.com>
2020-01-09 14:17:31 -0700

    Delete/Users3

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ app.post('/user', (req, res) => {
 app.delete('/user/:id', (req, res) => {
     try{
         if(users.length > 0){
+            users = [];
             res.status(200).json({
                 ok: true
             }) 
