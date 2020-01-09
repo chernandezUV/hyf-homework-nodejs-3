79a70911e1aff4839750cbbaa563a67a18cba75f
chernandezUV <chernandez@unitedvirtualities.com>
2020-01-08 17:40:58 -0700

    Endpoint GET/user:id

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ var express = require('express');
 var app = express();
 let users = [];
 
+//Create a Server that would answer to "/" with "Hello World"
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+//Create an endpoint that would answer to GET to /users with the list of users
 app.get('/users', (req, res) => res.json(users));
+
+//Create an endpoint to GET to /user/:id
+app.get('/user/:id', (req, res) => {
+    let user = users.find(user => user.id == req.params.id);
+    res.json(user);
+});
 
 app.listen(3000, function () {
   console.log('Server Running in port 3000!');
