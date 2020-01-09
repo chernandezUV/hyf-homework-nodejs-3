4114e87bc83236bb1725a6c4af2286ddb6d3b404
chernandezUV <chernandez@unitedvirtualities.com>
2020-01-08 17:27:42 -0700

    Endpoint GET/user

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
+let users = [];
 
+//Cree un servidor que responda a "/" con "Hello World"
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+//Cree un punto final que responda GETa /users con la lista de usuarios
+app.get('/users', (req, res) => res.json(users)); 
+
+//PORT 3000
 app.listen(3000, function () {
   console.log('Server Running in port 3000!');
 });
+
+
